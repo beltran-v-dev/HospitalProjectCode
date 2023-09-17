7e03a3b2e67c2b820f02c02a3a2dee4ce1d3867a
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-09-17 13:48:32 +0200

    Update Weapon.cpp

--- Weapon.cpp
+++ Weapon.cpp
@@ -3,3 +3,65 @@
 
 #include "Weapon.h"
 
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	Ammo = 32;
+	WeaponType = EWeaponType::EWT_Gun;
+	AmmoType = EAmmoType::EAT_GunAmmo;
+	ReloadMontageSection = FName(TEXT("Reload_Gun")); 
+	MagazineCapacity = 8;
+
+	MaxCapacity = MagazineCapacity;
+
+	
+
+}
+
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AWeapon::DecreaseAmmo()
+{
+	/*if (!(Ammo <= 0))
+	{
+		--Ammo;
+	}*/
+
+
+	if (!(MagazineCapacity <= 0))
+	{
+		--MagazineCapacity;
+	
+
+	
+	}
+
+	
+	
+	
+}
+
+void AWeapon::ReloadAmmo(int32 Ammount)
+{
+	//Revisar
+
+	//checkf(Ammo + Ammount <= MagazineCapacity, TEXT(""));
+	MagazineCapacity += Ammount;
+
+	//Ammo += Ammount;
+
+}
+
+int32 AWeapon::MaxCapacityMagazine() const
+{
+	
+
+	return MaxCapacity;
+}
+
+
+
+
