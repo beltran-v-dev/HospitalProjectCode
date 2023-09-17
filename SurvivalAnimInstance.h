68850a71102dec7fadba8fa786b4252836007fc6
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-09-17 13:47:41 +0200

    Update SurvivalAnimInstance.h

--- SurvivalAnimInstance.h
+++ SurvivalAnimInstance.h
@@ -1,382 +1,65 @@
-
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
-#include "AmmoType.h"
-#include "SurvivalCharacter.generated.h"
+#include "Animation/AnimInstance.h"
+#include "SurvivalAnimInstance.generated.h"
 
 
-UENUM(BlueprintType)
-enum class EComabtState : uint8
-{
-	ECS_NoWeapon UMETA(DisplayName = "NoWeapon"),
-	ECS_Unoccupied UMETA(DisplayName = "Unoccupied"),
-	ECS_FireTimerInProgress UMETA(DisplayName = "FireTimerInProgress"),
-	ECS_Reloading UMETA(DisplayName = "Reloading"),
 
 
-	ECS_MAX UMETA(DisplayName = "ECS_Max")
-
-};
 
 UCLASS()
-class HOSPITALPROJECT_API ASurvivalCharacter : public ACharacter
+class HOSPITALPROJECT_API USurvivalAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	ASurvivalCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-	//Called when FireButton is pressed
-	void FireWeapon();
-
-	void EnableFire();
-
-	//Line trace for item under the crosshairs
-	bool TraceUnderCorsshairs(FHitResult& OutHitResult);
-
-	//Trave for items if OverlappedItemCound > 0
-	void TraceForItems();
-
-
-	/**
-	*Rotate controller based on mouse X movement
-	@param Value: The input value from mouse movement
-	*/
-	void Turn(float Value);
-
-	/**
-	*Rotate controller based on mouse Y movement
-	@param Value: The input value from mouse movement
-	*/
-	void LookUp(float Value);
-
-	/**
-	* Crosshair spread will be calcualted every frame and to do that we are going to use interpolation
-	* becuase of that we are going to use a variable float DeltaTime to that propuse
-	*/
-	void CalculateCorsshairSpread(float DeltaTime);
-
-
-	//Functions to set ture/flase the bFiringBullet variable in orter to spread the corsshair when we the player shoots 
-	void FinishCrosshairBulletFire();	
-	void StartCorsshairBulletFire();
-
-	//Spawns a default weapon and equips it
-	class AWeapon* SpawnDefaultWeapon();
-
-	//Takes a weapon and attaches it to the mesh
-	void EquipWeapon(class AWeapon* WeponToEquip);
-
-	//Detach weapon and let it fall to the ground
-	//void DropWeapon();
-
-	void SelectButtonPressed();
-	void SelectButtonReleased();
-
-	void NoWeapon();
-
-
-	void IsJogging();
-	void IsNotJoggin();
-
-
-	void PlayerWeaponAtStart();
-
-	//Equips TraceHitItem.
-	void EquipNewWeapon(AWeapon* NewWeaponToEquip);
-
-
-	//Functions to take care of jogging as the player is aiming or not 
-
-	void SetIsNotJogging();
-
-	void SetIsJogging();
-
-	//Initialize the AmmoMap with ammo values
-	void InitializeAmmoMap();
-
-	//Check to make sure out wapon has ammo
-	bool WeaponHasAmmo();
-
-	//FireWeapon functions
-
-	void PlayFireSound();
-	void MuzzleFlashPlaced();
-	void PlayGunFireMontage();
-
-
-	//Bound to the R key
-	void ReloadButtonPressed();
-
-	//Handle reloading of the weapon
-	void ReloadWeapon();
 
 	UFUNCTION(BlueprintCallable)
-	void FinishReloading();
-
-	//Checks to see if we have ammo of the EquippedWeapon's ammo type
-	bool CarryingAmmo();
+	void UpdateAnimationProperties(float deltaTime);
 
 
-	
+//Function NativeInitializeAnimation that works like begin play bur for animationBlueprint
+	virtual void NativeInitializeAnimation() override;
 
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	//Called for forwards/backwards input
-	void MoveForwardBackward(float value);
-
-	//Called for right/left input
-	void MoveRightLeft(float value);
-
-	//The variable bIsAiming will be set to true/false every time this function is called 
-	void IsAiming();
-	//The variable bIsAiming will be set to true/false every time this function is called 
-	void IsNotAiming();
-
-	//Set camera FOV smoothly
-	void SetCameraFOV(float DeltaTime);
-
-	
-
-
-
-
-
-
-
-
-	//Variables
 private:
 
-	//Camera boom positioning the camera behind the character
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = true))
-		class USpringArmComponent* CameraBoom;
 
-	//Camera that follows the character
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = true))
-		class UCameraComponent* Camera;
+	//Vatiable to hold the SurvivalCharacter reference
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess="true"))
+	class ASurvivalCharacter* SurvivalCharacter;
 
-	//Randomized gunshot sound cue
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		class USoundCue* FireSound;
+	//Speed of the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float speed;
 
-	//Flash spawned at barrelSocket
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		class UParticleSystem* MuzzleFlash;
+	//To know if the character is accelerating
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
 
-	class USkeletalMeshComponent* MainSkeletalMesh;
+	//Direction used to blendspace
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Direction;
 
-	bool bIsShoot;
+	//Direction used to blendspace
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float DirectionBeforeStopping;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		float timeBetweenShootsGun;
-
-	//Montage for firing weapon
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		class UAnimMontage* HipFireMontage;
-
-	//Particles spawned upon bullent impact
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		UParticleSystem* impactParticles;
-
-	//Bool to set if our player is aiming or not
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
-		bool bIsAiming;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
-		bool bHasBeenAimingWalk;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
-		bool bHasBeenAiming;
-
-	//Distance which our player is going to be able to hit objects as they are shooting
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		float ShootingDistance;
-
-	//Defailt cmaera field of view value
-	float CameraDefaultFOV;
-
-	//Field of view value when we zoom in
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		float CameraZoomedFOV;
-
-	//Current field of view this frame
-	float CameraCurrentFOV;
-
-	//Interps speed for zooming when aiming
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
-		float ZoomInterpSpeed;
-
-
-	//Aiming speed when we are aiming or not
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMIN = "0.0", UIMAX = "1.0"))
-		float AimingSpeedWhenAimingUp;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMIN = "0.0", UIMAX = "1.0"))
-		float AimingSpeedWhenAimingSides;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMIN = "0.0", UIMAX = "1.0"))
-		float AimginSpeedWhenIsNotAimingUp;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true), meta = (ClampMin = "0.0", ClampMax = "1.0", UIMIN = "0.0", UIMAX = "1.0"))
-		float AimginSpeedWhenIsNotAimingSides;
-
-	//Bool which says if the gun has been equipped
-	bool bGunHasBeenEquipped;
-
-	//Bool to determinte if the key to fog has been pressed
-	bool bIsPressingTheJogginKey;
-
-	//crosshair spread
-
-	//variable to determinate the spread of the corsshairs
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Crosshairs, meta = (AllowPrivateAccess = true))
-	float CrosshairSpreadMuliplier;
-
-	//Variable to change the velocity spread in the CrosshairSpreadMuliplier variable when we hare moving around
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Crosshairs, meta = (AllowPrivateAccess = true))
-	float CorsshairVelocityFactor;
-
-	//Variable to change the velocity spread in the CrosshairSpreadMuliplier variable when we are aiming
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Crosshairs, meta = (AllowPrivateAccess = true))
-	float CrosshairAimFactor;
-
-	//Variable to change the velocity spread in the CrosshairSpreadMuliplier variable when we are shooting
-	float CrosshairShootingFactor;
-	
-	//Endcrosshair spread
-	
-	//Variable to check if our player has shooted or not to controll the corsshair shooting movement
-	bool bFiringBullet;
-
-	//True if we should trace every frame for item
-	bool bShouldTraceForItems;
-
-	//Number of overlapped AItems
-	int8 OverlappedItemCount;
-
-	//The AItem we hit last frame
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= Items, meta = (AllowPrivateAccess = "true"))
-	class AItem* TraceHitItemLastFrame;
-
-	//Currently equipped weapon
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
-	class AWeapon* EqquipedWeapon;
+	bool bIsAiming;
 
-	//Set this in Blueprints for the default weapon class
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
-	TSubclassOf<AWeapon> DefaultWeaponClass;
-
-	//Montage to equip the gun
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
-	class UAnimMontage* EquipGunMontage;
-
-	//Montage to equip the gun
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
-	class UAnimMontage* PullsOutGunMontage;
-
-	//Bool to check if our player has an weapon equipped 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
 	bool bHasAWeapon;
 
-	//Bool to check if the player is jogging 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
 	bool bIsJogging;
-	
-	//Float to indicate the character velocity when it's walking
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float WalkingSpeed;
 
-	//Float to indicate the character velocity when it's Jogging
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	float JoggingSpeed;
-
-	//The Item currently hit by our trauce in TraceForIntems() (it could be null)
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
-	AItem* TraceHitItem;
-
-	//Map to keep track of ammo of different ammo types
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Items, meta = (AllowPrivateAccess = "true"))
-	TMap<EAmmoType, int32> AmmoMap;
-
-	//Starting amount gun ammo
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Items, meta = (AllowPrivateAccess = "true"))
-	int32 StartingGunAmmo;
-
-	//Starting amount riffle ammo
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Items, meta = (AllowPrivateAccess = "true"))
-	int32 StartingRiffleAmmo;
-
-	//Combat State, can only fire or reload if Unoccupied
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
-	 EComabtState CombatState;
-	
-	//Montage for reaload Animations 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
-	UAnimMontage* ReloadMontage;
-
-	
 
 
 public:
 
-
-
-	//Returns CameraBoom subObject
-	FORCEINLINE USpringArmComponent* GetCameraBoom() const
-	{
-		return CameraBoom;
-	}
-
-	//Returns CameraComponent subObject
-	FORCEINLINE UCameraComponent* GetCamera() const
-	{
-		return Camera;
-	}
-
-	FORCEINLINE bool GetAimingBool()  const
-	{
-		return bIsAiming;
-	}
-
-	UFUNCTION(BlueprintCallable)
-	float GetCrosshairSpreadMultiplier();
-
-	FORCEINLINE int8 GetOverlapappedItemCound()
-	{
-		return OverlappedItemCount;
-	}
-
-	//Add/subtract to/from overlappedItemCount and updates bShouldTraceFromItems
-	void IncrementOverlappedItemCount(int8 Amount);
-
-
-
-	FORCEINLINE bool GetbHasAWeapon()
-	{
-		return bHasAWeapon;
-	}
-
-	FORCEINLINE bool GetbIsJoging()
-	{
-		return bIsJogging;
-	}
-
-
+	
+	
 };
