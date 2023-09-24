374511218e992dfafa5f6a2d163e75819f4a90b1
Beltrán <123870745+beltran-v-dev@users.noreply.github.com>
2023-09-24 10:16:38 +0200

    Update Weapon.h

--- Weapon.h
+++ Weapon.h
@@ -4,7 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "Item.h"
+#include "AmmoType.h"
 #include "Weapon.generated.h"
+
+
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	EWT_NoWeapon UMETA(DisplayName = "NoWeapon"),
+	EWT_Rope UMETA(DisplayName = "Rope"),
+	EWT_Gun UMETA(DisplayName = "Gun"),
+	EWT_Riffle UMETA(DisplayName = "Riffle"),
+
+	EWT_MAX UMETA(DisplayName = "DefaultMax")
+};
 
 /**
  * 
@@ -14,4 +27,79 @@ class HOSPITALPROJECT_API AWeapon : public AItem
 {
 	GENERATED_BODY()
 	
+public:
+	AWeapon();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+private:
+
+	//Max ammo that our weapon can hold
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	int32 Ammo;
+
+	//Maximum ammo that every magazine can hold
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	int32 MagazineCapacity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	int32 MaxCapacity;
+
+	
+	//Type of weapon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	EWeaponType WeaponType;
+
+	//Type of ammo for this weapon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	EAmmoType AmmoType;
+
+	//FName for the Reload Montage section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	FName ReloadMontageSection;
+
+
+	
+
+
+
+public:
+
+	FORCEINLINE int32 GetAmmo()
+	{
+		return Ammo;
+	}
+
+	FORCEINLINE  int32 GetMagazineCapacity()
+	{
+		return MagazineCapacity;
+	}
+
+	//Called from character class when firing weapon
+	void DecreaseAmmo();
+
+
+	FORCEINLINE EWeaponType GetWeaponType()
+	{
+		return WeaponType;
+	}
+
+	FORCEINLINE EAmmoType GetAmmoType()
+	{
+		return AmmoType;
+	}
+
+	FORCEINLINE FName GetReloadMontageSection()
+	{
+		return ReloadMontageSection;
+	}
+
+	void ReloadAmmo(int32 Ammount);
+
+	 int32 MaxCapacityMagazine() const;
+
+	 bool IsCarryingMaxCapacity();
+
 };
